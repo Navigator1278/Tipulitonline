c7e787fee5ada4770b43fb57654d9a706a5be105
skachkov <skachkov@guns.ru>
2011-03-05 23:06:17 +0200

    sever fix; branch 'production1' implemented;

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+#defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__FILE__) . '/application');
 
 //// TODO uncomment for production
 //// Define application environment
@@ -15,10 +15,8 @@ defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
+#set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'),get_include_path(),)));
+set_include_path(implode(PATH_SEPARATOR, array(APPLICATION_PATH . '/../library',get_include_path(),)));
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
