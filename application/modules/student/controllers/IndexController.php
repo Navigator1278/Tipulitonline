36b67a648092eb7ba0ba8b1fdc6bf43316e06431
skachkov <skachkov@guns.ru>
2011-03-09 15:55:27 +0200

    layout fix

--- application/modules/student/controllers/IndexController.php
+++ application/modules/student/controllers/IndexController.php
@@ -5,8 +5,11 @@ class Student_IndexController extends Zend_Controller_Action
 
     public function  preDispatch()
     {
+    	# turn on application/layouts/scripts/student.phtml
+        $this->_helper->layout->setLayout('student');
+
         $auth = Zend_Auth::getInstance();
-        if (!$auth->hasIdentity()) $this->_redirect ('/');
+        #if (!$auth->hasIdentity()) $this->_redirect ('/');
     }
 
     public function init()
@@ -16,8 +19,9 @@ class Student_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
+
        //  $this->_helper->viewRenderer->setNoRender();
-        $this->_helper->layout()->disableLayout();
+       # $this->_helper->layout()->disableLayout();
     }
 
 
